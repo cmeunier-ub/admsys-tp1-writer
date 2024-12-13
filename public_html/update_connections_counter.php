a2a0015f76f255eb232954c45e7c5047ddf0f940
Les Moulins du Dev <lesmoulinsdudev@gmail.com>
2024-12-13 20:46:58 +0100

    Write the query on a single line

--- public_html/update_connections_counter.php
+++ public_html/update_connections_counter.php
@@ -4,7 +4,6 @@ require_once("./../config.php");
 
 $db = new PDO("mysql:host=$DB_HOST:$DB_PORT;dbname=$DB_NAME", $DB_USER, $DB_PASS);
 
-$statement = $db->prepare("INSERT INTO connections (`date`, `number`) VALUES(now(), 1) ON DUPLICATE KEY UPDATE    
-`number`=`number`+1");
+$statement = $db->prepare("INSERT INTO connections (`date`, `number`) VALUES(now(), 1) ON DUPLICATE KEY UPDATE `number`=`number`+1");
 
 $statement->execute();
